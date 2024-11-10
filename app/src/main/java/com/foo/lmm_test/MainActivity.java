4cf76fa2cb9347b3573a8d9484a473a2260da473
Jeonseoknam <sukkzz7065se@gmail.com>
2024-11-10 23:11:34 +0900

    first test

---
+++ app/src/main/java/com/foo/lmm_test/MainActivity.java
@@ -0,0 +1,25 @@
+package com.foo.lmm_test;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.foo.lmm_test.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //멤버 전석남이 push한 최초의 코드
+    }
+}
